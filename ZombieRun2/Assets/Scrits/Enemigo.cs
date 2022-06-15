32573ba5e48592f04f86a4eac96a63f3250170fa
unknown <SENA@MEDAPRCCSFSD337.FormacionSena.RED>
2022-06-15 17:39:50 -0500

    nuevo

--- ZombieRun2/Assets/Scrits/Enemigo.cs
+++ ZombieRun2/Assets/Scrits/Enemigo.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Enemigo : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector3 direccion;
+    public float speedMovement;
+
     void Start()
     {
-        
+        direccion.y = -1;
+
+        transform.Translate(direccion);
+
+        Debug.Log("hsdakjds");
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         

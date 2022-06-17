2ae5abdcc48dcecb0571247f0726d8ebc1fa40e1
unknown <SENA@MEDAPRCCSFSD337.FormacionSena.RED>
2022-06-17 17:35:32 -0500

    Spawnea cada cierto tiempo, zigzagea

--- ZombieRun2/Assets/Scrits/SpawnZom.cs
+++ ZombieRun2/Assets/Scrits/SpawnZom.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class SpawnZom : MonoBehaviour
 {
-    public GameObject prefab;
-    
+    public GameObject Prefab;
+    public float TiempoDeCreacion = 2f, rangoDeCreacion = 2f; 
     private float cont;
+
+     void Start()
+     {
+        InvokeRepeating("Creando", 0.0f, TiempoDeCreacion);  
+     }
+
     void Update()
     {
         if (cont >= 2)
         {
-            GameObject GO = Instantiate(prefab, transform.position, transform.rotation);
+            GameObject GO = Instantiate(Prefab, transform.position, transform.rotation);
             cont = 0;
         }
 
         cont += Time.deltaTime;
     }
+     
+    public void Creando()
+    {
+        Vector3 spawnPosition = new Vector3(0, 0, 0);
+        spawnPosition = this.transform.position + Random.onUnitSphere * rangoDeCreacion;
+        spawnPosition = new Vector3(spawnPosition.x, spawnPosition.y, 0);
+
+        GameObject prefab = Instantiate(Prefab, spawnPosition, Quaternion.identity);
+
+    }
+    
 }
